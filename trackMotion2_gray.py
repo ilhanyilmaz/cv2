bf0c5250afaeb83995174edea69cbe2b28541333
ILHAN YILMAZ <ilhannn@gmail.com>
2014-11-24 16:50:21 +0200

    Changes to be committed: 	modified:   trackMotion2_gray.py

--- trackMotion2_gray.py
+++ trackMotion2_gray.py
@@ -15,6 +15,9 @@ else :
 
 backImage = cv2.imread("backgroundImageGray.jpg", cv2.COLOR_BGR2GRAY)
 
+kernel = np.ones((3,3),np.uint8)
+
+
 while(capture.isOpened): 
     f,img=capture.read()
 
@@ -22,8 +25,12 @@ while(capture.isOpened):
         imgGray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         diffImage = cv2.absdiff(backImage,imgGray)
         ret, threshold = cv2.threshold(diffImage, 20, 255, cv2.THRESH_BINARY)
+        threshold = cv2.erode(threshold,kernel,iterations = 1)
+        threshold = cv2.dilate(threshold,kernel,iterations = 3)
+        contours, hierachy = cv2.findContours(threshold, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(img,contours,-1,(0,255,0),3)
 
-        cv2.imshow('track', threshold)
+        cv2.imshow('track', img)
 
     if(cv2.waitKey(27)!=-1):
         capture.release()
