90324f82885e00c38c981a7baf2e6aa731057165
ILHAN YILMAZ <ilhannn@gmail.com>
2014-11-24 10:15:45 +0200

    Added video support to trackMotion.py

--- trackMotion.py
+++ trackMotion.py
@@ -1,17 +1,28 @@
 import cv2
 import numpy as np
+import sys
 
-cam=cv2.VideoCapture(0)
+capture = None
+inputFile = None
+
+if len(sys.argv) > 1 :
+    inputFile = sys.argv[1]
+    print "Opening file: {}".format(inputFile)
+    capture=cv2.VideoCapture(inputFile)
+else :
+    print "Opening camera."
+    capture=cv2.VideoCapture(0)
+
 fgbg = cv2.BackgroundSubtractorMOG()
 
-while(cam.isOpened): 
-   f,img=cam.read()
-   if f==True:
-       #img=cv2.flip(img,1)
-       #img=cv2.medianBlur(img,3)
-       fgmask = fgbg.apply(img)
-       cv2.imshow('track',fgmask)
-   if(cv2.waitKey(27)!=-1):
-       cam.release()
-       cv2.destroyAllWindows()
-       break 
+while(capture.isOpened): 
+    f,img=capture.read()
+    if f==True:
+        #img=cv2.flip(img,1)
+        #img=cv2.medianBlur(img,3)
+        fgmask = fgbg.apply(img)
+        cv2.imshow('track',fgmask)
+    if(cv2.waitKey(27)!=-1):
+        capture.release()
+        cv2.destroyAllWindows()
+        break 
